8b3445c585bbe9abf24bb0bd9004094999dbba9e
Johannes-J-J <jj3912@srmist.edu.in>
2026-03-02 14:03:35 +0530

    feat: implement UC1 - print literal OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp - UC1: Foundational setup to print literal text.
+ * @author Johannes Joby John
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application.
+     * Uses System.out.println() to send a String Literal to the console.
+     */
+    public static void main(String[] args) {
+        // Core Task: Print the literal text "OOPS"
+        System.out.println("OOPS");
+    }
+}
